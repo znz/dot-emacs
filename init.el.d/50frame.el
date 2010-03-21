af014eb2b567b32ff005a0d21c48158560e64386
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-22 04:47:23 +0900

    tool-bar-mode

--- init.el.d/50frame.el
+++ init.el.d/50frame.el
@@ -4,6 +4,10 @@
       (menu-bar-mode 1)
     (menu-bar-mode -1)))
 
+;; ツールバーを表示しない
+(static-when (fboundp 'tool-bar-mode)
+  (tool-bar-mode 0))
+
 ;;; 常に折り返し表示する
 ;;; (左右のスクロールは C-x< と C-x>
 ;; window 分割時、画面外に出る文章を折り返したい
