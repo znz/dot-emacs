995891e871e1b07c5406cbb3b0217e8ecc04fecd
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-22 06:14:15 +0900

    fix encoding

--- init.el.d/50scroll.el
+++ init.el.d/50scroll.el
@@ -1,10 +1,10 @@
-;;; Emacs: $B%+!<%=%k$rF0$+$5$:$K%9%/%m!<%k(B
+;;; Emacs: カーソルを動かさずにスクロール
 (defun my-scroll-down-1 ()
-  "$B%+!<%=%k$rF0$+$5$:$K%9%/%m!<%k(B"
+  "カーソルを動かさずにスクロール"
   (interactive)
   (scroll-down 1))
 (defun my-scroll-up-1 ()
-  "$B%+!<%=%k$rF0$+$5$:$K%9%/%m!<%k(B"
+  "カーソルを動かさずにスクロール"
   (interactive)
   (scroll-up 1))
 (put 'my-scroll-down-1 'unscrollable t)
@@ -12,9 +12,9 @@
 (define-key global-map "\M-p" 'my-scroll-down-1)
 (define-key global-map "\M-n" 'my-scroll-up-1)
 
-(setq next-screen-context-lines 10) ;; $B2hLL%9%/%m!<%k$G=EJ#I=<($5$l$k9T?t(B
+(setq next-screen-context-lines 10) ;; 画面スクロールで重複表示される行数
 
-;; 1$B9T$:$D%9%/%m!<%k$9$k$h$&$K$9$k!#(B
+;; 1行ずつスクロールするようにする。
 ;; http://pc2.2ch.net/test/read.cgi/software/1056601345/463
 (setq scroll-conservatively 35)
 (setq scroll-margin 2)
@@ -22,6 +22,6 @@
 
 ;;; Local Variables:
 ;;; mode: emacs-lisp
-;;; coding: iso-2022-7bit
+;;; coding: utf-8
 ;;; indent-tabs-mode: nil
 ;;; End:
