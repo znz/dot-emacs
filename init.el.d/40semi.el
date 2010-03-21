f4f7f1b0119869ee44211d70b3afbed5bff56652
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-21 10:29:04 +0900

    setup semi

---
+++ init.el.d/40semi.el
@@ -0,0 +1,17 @@
+;; HTML パートを表示しない
+;; mime-setup がロードされる前に記述する必要があります。
+(setq mime-setup-enable-inline-html nil)
+
+;; http://triaez.kaisei.org/~kaoru/diary/?200111c#200111252
+;; SEMI でファイル挿入時にカーソル移動ができなくなったような錯覚 を防止する。
+(add-hook
+ 'mime-edit-mode-hook
+ #'(lambda ()
+     (setq buffer-invisibility-spec '((t . t)))
+     (set (make-local-variable 'line-move-ignore-invisible) t)))
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
