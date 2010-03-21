d928ccf0b1fe8afc8419fa4454da8c6f1fa7635e
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-22 02:08:05 +0900

    history

---
+++ init.el.d/50history.el
@@ -0,0 +1,20 @@
+;; file-name-historyなどの数
+(setq history-length 1000)
+
+;; http://homepage3.nifty.com/akima/soft/meadow_20.html#SEC293
+;; 20.2.5 ヒストリから重複を削除 (2003/01/21)
+;; history から重複したのを消す
+(eval-when-compile (require 'cl))
+(defun minibuffer-delete-duplicate ()
+  (let (list)
+    (dolist (elt (symbol-value minibuffer-history-variable))
+      (unless (member elt list)
+        (push elt list)))
+    (set minibuffer-history-variable (nreverse list))))
+(add-hook 'minibuffer-setup-hook 'minibuffer-delete-duplicate)
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
