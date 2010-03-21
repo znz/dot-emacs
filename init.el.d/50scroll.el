eff1e2f5f593abce58fbbc044924f870aa097f05
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-22 04:28:38 +0900

    scroll

---
+++ init.el.d/50scroll.el
@@ -0,0 +1,27 @@
+;;; Emacs: カーソルを動かさずにスクロール
+(defun my-scroll-down-1 ()
+  "カーソルを動かさずにスクロール"
+  (interactive)
+  (scroll-down 1))
+(defun my-scroll-up-1 ()
+  "カーソルを動かさずにスクロール"
+  (interactive)
+  (scroll-up 1))
+(put 'my-scroll-down-1 'unscrollable t)
+(put 'my-scroll-up-1 'unscrollable t)
+(define-key global-map "\M-p" 'my-scroll-down-1)
+(define-key global-map "\M-n" 'my-scroll-up-1)
+
+(setq next-screen-context-lines 10) ;; 画面スクロールで重複表示される行数
+
+;; 1行ずつスクロールするようにする。
+;; http://pc2.2ch.net/test/read.cgi/software/1056601345/463
+(setq scroll-conservatively 35)
+(setq scroll-margin 2)
+(setq scroll-step 1)
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: iso-2022-7bit
+;;; indent-tabs-mode: nil
+;;; End:
