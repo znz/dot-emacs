b6cfa973bbd3490cd8032f379e77fda8b0ebd7f8
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-22 02:08:17 +0900

    kill ring

---
+++ init.el.d/50kill.el
@@ -0,0 +1,11 @@
+;; カーソル行頭時に C-kで行全体を削除
+(setq kill-whole-line t)
+
+;; kill ring に関する設定
+(setq kill-ring-max 1000) ; default=60
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
