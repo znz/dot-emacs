9de1640816f8b486f100fb5ac9063cc73e40eb97
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-22 03:00:05 +0900

    perl

---
+++ init.el.d/50perl.el
@@ -0,0 +1,23 @@
+(defun perldoc (word)
+  (interactive "sperldoc: ")
+  (let ((manual-program))
+    (setq manual-program "perldoc")
+    (Man-getpage-in-background word)))
+
+(defun my-perl-mode-hook ()
+  (set (make-local-variable 'compile-command)
+        (concat "perl -cw " buffer-file-name))
+  )
+(add-hook 'perl-mode-hook #'my-perl-mode-hook)
+(add-hook 'cperl-mode-hook #'my-perl-mode-hook)
+
+;; http://d.hatena.ne.jp/odz/20070528/1180331830
+(defun perltidy-region (beg end)
+  (interactive "r")
+  (shell-command-on-region beg end "perltidy -q" nil t))
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
