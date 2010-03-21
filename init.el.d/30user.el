868f02673dc71277bc7a117801d764a268dc3e0e
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-21 10:27:23 +0900

    set user-full-name and user-mail-address

---
+++ init.el.d/30user.el
@@ -0,0 +1,14 @@
+(defconst my-user-full-name user-full-name
+  "元のuser-full-name")
+(setq user-full-name "Kazuhiro NISHIYAMA")
+(defconst my-user-mail-address user-mail-address
+  "元のuser-mail-address")
+(setq user-mail-address
+      (or (getenv "EMAIL")
+          "zn\@mbf.nifty.com"))
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
