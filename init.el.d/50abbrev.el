bf47aef29a15dd868350fe521c1cd49605dd26c0
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-21 10:29:19 +0900

    setup abbrev

---
+++ init.el.d/50abbrev.el
@@ -0,0 +1,10 @@
+(setq abbrev-file-name
+      (expand-file-name (concat "abbrev_defs.el")
+                        temporary-file-directory))
+(setq save-abbrevs 'silently)
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
