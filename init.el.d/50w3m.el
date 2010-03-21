6a67ff400132511eba1ce591c9e5d814adf9d5e0
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-22 06:03:33 +0900

    emacs-w3m

---
+++ init.el.d/50w3m.el
@@ -0,0 +1,16 @@
+(static-when (locate-library "w3m-load")
+  (require 'w3m-load)
+  (setq w3m-init-file "~/.emacs.d/dot-emacs-w3m")
+  (eval-after-load "w3m" '(my-byte-compile-file w3m-init-file))
+
+  (setq w3m-type "w3m")
+
+  ;;(unless (featurep 'meadow)
+  ;;  (setq browse-url-browser-function 'w3m-browse-url))
+  )
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
