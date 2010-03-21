93e6cb7e88fa5446ecf93736d151368def617287
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-21 10:30:26 +0900

    setup auto-save

---
+++ init.el.d/50auto-save.el
@@ -0,0 +1,16 @@
+;;; .save.. ってなファイルを作らない
+;;(setq auto-save-list-file-name nil)
+;;(setq auto-save-list-file-prefix nil)
+
+(setq auto-save-list-file-prefix
+      (expand-file-name "auto-save/" temporary-file-directory))
+(make-directory auto-save-list-file-prefix t)
+
+;; 何文字毎に自動保存するか
+(setq auto-save-interval 180)
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
