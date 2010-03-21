ac9482a75ae016e6343451422d26205b530934dd
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-22 04:10:27 +0900

    navi2ch

--- init.el.d/50navi2ch.el
+++ init.el.d/50navi2ch.el
@@ -3,7 +3,7 @@
     (and
      (file-directory-p "~/.navi2ch")
      (locate-library "navi2ch"))
-  (setq navi2ch-init-file "~/.emacs.d/init-navi2ch")
+  (setq navi2ch-init-file "~/.emacs.d/dot-navi2ch")
   (eval-after-load "navi2ch" '(my-byte-compile-file navi2ch-init-file))
   (autoload 'navi2ch "navi2ch" "Navigator for 2ch for Emacs" t nil))
 
