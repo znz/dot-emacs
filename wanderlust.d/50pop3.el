b6ab84843905489dcd36d020689d371bfc0d6ca9
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-22 13:05:12 +0900

    elmo-pop3

---
+++ wanderlust.d/50pop3.el
@@ -0,0 +1,11 @@
+;; POP サーバの設定
+(setq elmo-pop3-default-server "localhost")
+
+;; POP-before-SMTP
+;;(setq wl-draft-send-mail-function 'wl-draft-send-mail-with-pop-before-smtp)
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
