1caff77b366f333c0bf3cc05555e21d6109b9d79
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-22 13:04:04 +0900

    elmo-msgdb

---
+++ wanderlust.d/50msgdb.el
@@ -0,0 +1,27 @@
+(setq elmo-msgdb-default-type 'standard)
+
+;; msgdb をロードした時に `elmo-msgdb-default-type' と
+;; 異なった形式だった場合に自動的に変換する場合
+(setq elmo-msgdb-convert-type 'auto)
+
+;; サマリで s all した時に変換する場合
+;(setq elmo-msgdb-convert-type 'sync)
+
+;; `elmo-msgdb-overview-entity-get-extra-field' で参照したいフィールド。
+;; 自動リファイルで参照したいフィールドも設定する。
+(setq elmo-msgdb-extra-fields
+      '("newsgroups"
+        "content-type"
+        "reply-to"
+        ;; ML
+        "list-id" "x-ml-name" "mailing-list"
+        "x-mail-count" "x-ml-count" "x-sequence"
+        ;; redmine
+        "x-redmine-project"
+        ))
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
