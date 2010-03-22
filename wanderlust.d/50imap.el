b23259e2a697fd707435ab3c9a1c977d78555068
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-22 13:03:47 +0900

    elmo-imap4

---
+++ wanderlust.d/50imap.el
@@ -0,0 +1,12 @@
+(setq elmo-imap4-debug nil)
+
+; フォルダ名が省略されないように現在のユーザ名を使わないで、ダミーの名前にしておく。
+(setq elmo-imap4-default-user "imap4-default-user")
+
+(setq elmo-imap4-use-modified-utf7 t)
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
