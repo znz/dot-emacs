53e2eebe7fc1614e37a668b492df06cdb0427a4a
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-03-22 13:04:49 +0900

    elmo-nntp

---
+++ wanderlust.d/50nntp.el
@@ -0,0 +1,21 @@
+(setq elmo-nntp-group-coding-system 'utf-8)
+
+(setq elmo-nntp-default-server "localhost")
+(setq elmo-nntp-default-user nil)
+(setq elmo-nntp-default-port 119)
+
+;; .folders の記述例:
+;; -gmane.comp.lang.ruby.devel@news.gmane.org
+;; -gmane.comp.lang.ruby.japanese@news.gmane.org
+
+(setq wl-nntp-posting-config-alist
+      (list
+       '(",?gmane\\." . "news.gmane.org")
+       '(",?ring\\." . "news.ring.gr.jp")
+       ))
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
