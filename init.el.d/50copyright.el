a79e17cfc70b04641055bbc3e72e02c8b2845448
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-04-07 03:40:23 +0900

    don't touch GPL version

--- init.el.d/50copyright.el
+++ init.el.d/50copyright.el
@@ -1,6 +1,11 @@
 ;; copyright の年や GPL のバージョンを更新
 (static-when (fboundp 'copyright-update)
+  ;; 自作のものだけ更新する。
   (setq copyright-names-regexp (regexp-quote user-full-name))
+  ;; GPL のバージョンはいじらない。
+  (defadvice copyright-update (around force-physical-line activate)
+    (let ((copyright-current-gpl-version nil))
+      ad-do-it))
   (add-hook 'write-file-hooks 'copyright-update))
 
 ;;; Local Variables:
