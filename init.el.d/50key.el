571f237b6e57cdb36aa892391544e6bc1df3ee2d
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-04-13 10:46:52 +0900

    cleanup comments

--- init.el.d/50key.el
+++ init.el.d/50key.el
@@ -1,22 +1,19 @@
 ;;; 一般的なキー設定。
-;;; 指定行へ移動
+;; 指定行へ移動
 (global-set-key "\M-g" 'goto-line)
 
 ;; C-hもBackSpaceも同じ挙動にする。
 (keyboard-translate ?\C-h ?\C-?)  ; translate `C-h' to DEL
 ;;(keyboard-translate ?\C-? ?\C-h)  ; translate DEL to `C-h'.
 
-;; 1 回の delete-backward-char で複数の whitespace を消したい
-;; http://www.fan.gr.jp/~ring/Meadow/meadow.html#backward-delete-char-untabify-method
+;; 1 回の delete-backward-char で複数の whitespace を消す。
 (setq backward-delete-char-untabify-method 'hungry)
 
 (global-set-key "\M- " 'just-one-space) ; デフォルトと同じはず
 (global-set-key "\M-q" 'fill-paragraph) ; 段落整形
 (global-set-key "\M-h" 'help-command)   ; C-h の代わり
 (global-set-key [f1] 'help-command)     ; C-h の代わり
-;; [f2] は dmacro
 (global-set-key [(control f2)] 'sort-lines)
-;;(global-set-key [f3] 'wl)               ; Wanderlust を起動
 (global-set-key [f4] 'toggle-read-only) ; 読み込み専用との切り替え
 (global-set-key [(control f4)] 'kill-this-buffer) ; バッファを削除
 (global-set-key [(meta f4)] 'delete-frame)   ; フレームを削除
@@ -45,8 +42,8 @@
     (open-line n))
 (global-set-key "\C-o" 'my-open-line)
 
-;;; C-xo とは逆向きへのウィンドウ移動
-;(global-set-key "\C-xp" (lambda () (interactive) (other-window -1)))
+;; C-xo とは逆向きへのウィンドウ移動
+;;(global-set-key "\C-xp" (lambda () (interactive) (other-window -1)))
 (defun my-other-window-back (arg) (interactive "p") (other-window (- arg)))
 (global-set-key "\C-xp" 'my-other-window-back)
 
