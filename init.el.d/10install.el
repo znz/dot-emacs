34e9e013d31b3ddb789dd5559cbf7f617ba96dbd
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-05-02 04:38:30 +0900

    add initialize commands in comment

--- init.el.d/10install.el
+++ init.el.d/10install.el
@@ -1,4 +1,8 @@
 (setq auto-install-directory "~/.emacs.d/elisp/")
+;; (make-directory auto-install-directory)
+;; (shell-command-to-string (concat "cd " auto-install-directory " && wget -N http://www.emacswiki.org/emacs/download/auto-install.el"))
+;; (shell-command-to-string (concat "cd " auto-install-directory " && curl -O http://www.emacswiki.org/emacs/download/auto-install.el"))
+;; (byte-compile-file (expand-file-name "auto-install.el" auto-install-directory))
 (static-when (file-directory-p auto-install-directory)
   (add-to-list 'load-path auto-install-directory)
   (require 'auto-install)
