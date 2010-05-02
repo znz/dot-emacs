cc94100c514b29ebc8cbeee44f0793a18ae9c69a
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-05-02 21:23:30 +0900

    transient-mark-mode

---
+++ init.el.d/50mark.el
@@ -0,0 +1,9 @@
+;; emacs23 のデフォルトの挙動にあわせる。
+(transient-mark-mode 1)
+(setq mark-even-if-inactive t)
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
