50a10e4af190a9055071ede0aa2ebab0f1b32aa0
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-05-07 12:58:14 +0900

    setup bbdb

---
+++ init.el.d/50bbdb.el
@@ -0,0 +1,17 @@
+;; BBDB
+(static-when (require 'bbdb nil t)
+  (setq bbdb-file (expand-file-name "_bbdb.el" user-emacs-directory))
+  (setq bbdb-north-american-phone-numbers-p nil)
+  (setq bbdb-use-pop-up t)
+  (setq bbdb-pop-up-target-lines 3)
+  (setq bbdb-offer-save 'savenoprompt)
+  (setq bbdb/mail-auto-create-p 'bbdb-ignore-some-messages-hook)
+  (setq bbdb-ignore-some-messages-alist
+        '(( "From" . "no.?reply\\|DAEMON\\|[Dd]aemon\\|root\\|logcheck\\|[Aa]rpwatch")))
+  )
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
