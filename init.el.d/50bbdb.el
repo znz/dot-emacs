7800f3a981d2cba5a77fe38b146275e51557c8bf
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-05-07 16:08:47 +0900

    setup bbdb

--- init.el.d/50bbdb.el
+++ init.el.d/50bbdb.el
@@ -5,9 +5,25 @@
   (setq bbdb-use-pop-up nil)
   (setq bbdb-pop-up-target-lines 3)
   (setq bbdb-offer-save 'savenoprompt)
+  (setq bbdb-quiet-about-name-mismatches t)
   (setq bbdb/mail-auto-create-p 'bbdb-ignore-some-messages-hook)
   (setq bbdb-ignore-some-messages-alist
-        '(( "From" . "no.?reply\\|DAEMON\\|[Dd]aemon\\|root\\|logcheck\\|[Aa]rpwatch")))
+        '(("From" . "no.?reply\\|DAEMON\\|daemon\\|root\\|master\\|admin\\|info\\|log\\|arpwatch")
+          ("To" . "DAEMON\\|daemon\\|root\\|master\\|admin\\|info\\|log\\|arpwatch")))
+  (add-hook 'bbdb-notice-hook 'bbdb-auto-notes-hook)
+  (setq bbdb-auto-notes-alist
+        '(("X-ML-Name" (".*$" ML 0))
+          ("X-Mailing-List" (".*$" ML 0))
+          ("X-Sequence" ("^\\([^ ]+\\) \\([0-9]+\\)$" ML 1))
+          ("List-Id" ("<\\([^<>]+\\)>" ML 1))
+          ("X-URL" (".*" www 0))
+          ("X-URI" (".*" www 0))
+          ("Organization" (".*" Organization 0))
+          ("Newsgroup" ("[^,]+" newsgroups 0))
+          ("X-Mailer" (".*$" User-Agent 0))
+          ("X-Newsreader" (".*$" User-Agent 0))
+          ("User-Agent" (".*$" User-Agent 0))
+          ))
   )
 
 ;;; Local Variables:
--- wanderlust.d/50bbdb.el
+++ wanderlust.d/50bbdb.el
@@ -1,7 +1,8 @@
 ;; BBDB
 (static-when (and (locate-library "bbdb") (locate-library "bbdb-wl"))
   (require 'bbdb-wl)
-  (setq bbdb-wl-ignore-folder-regexp "^[-$&@']\\|^\\+\\(draft\\|lost\\+found\\|outbox\\|queue\\|sent\\|trash\\)")
+  (setq bbdb-wl-ignore-folder-regexp
+        "^[-$&@']\\|^\\+\\(draft\\|lost\\+found\\|outbox\\|queue\\|sent\\|trash\\)\\|^[%].*\\(info\\|junk\\|log\\|root\\|spam\\)")
   (bbdb-wl-setup))
 
 ;;; Local Variables:
