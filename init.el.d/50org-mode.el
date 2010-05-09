37bd14481e18dc1c277ea02555be52077e4dd3bd
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-05-09 21:54:25 +0900

    update org-remember-templates

--- init.el.d/50org-mode.el
+++ init.el.d/50org-mode.el
@@ -18,11 +18,29 @@
   (org-remember-insinuate)
   (setq org-default-notes-file
         (expand-file-name "notes.org" org-directory))
-  (setq org-remember-templates
-      '(("Todo" ?t "** TODO %?\n   %i\n   %a\n   %t" nil "Inbox")
-        ("Bug" ?b "** TODO %?   :bug:\n   %i\n   %a\n   %t" nil "Inbox")
-        ("Idea" ?i "** %?\n   %i\n   %a\n   %t" nil "New Ideas")
-        ))
+  (let* ((body "\n   Added: %U\n   %a\n   %i")
+         (note (concat "** %?" body)))
+    (setq org-remember-templates
+          `(("Todo" ?t
+             ,(concat "** TODO %?" body)
+             "newgtd.org" "Inbox")
+            ("Bug" ?b
+             ,(concat "** TODO %?   :bug:" body)
+             "newgtd.org" "Inbox")
+            ("Work" ?w
+             ,(concat "** TODO %?   :work:" body)
+             "newgtd.org" "Inbox")
+            ("Home" ?h
+             ,(concat "** TODO %?   :home:" body)
+             "newgtd.org" "Inbox")
+
+            ("Event" ?e ,note "schedule.org" "Events")
+            ("Scheduling" ?s ,note "schedule.org" "Tasks")
+
+            ("Memo" ?m ,note nil "Memo")
+            ("Idea" ?i ,note nil "New Ideas")
+            ("GoodThing" ?g ,note nil "Good Things")
+            )))
 
   (setq org-log-done t)
   (setq org-startup-truncated nil)
