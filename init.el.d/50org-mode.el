7fb739ac382db8869d8aa9986af6e465c30e638d
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-05-09 20:55:40 +0900

    add org-remember

--- init.el.d/50org-mode.el
+++ init.el.d/50org-mode.el
@@ -5,9 +5,24 @@
 (static-when (locate-library "org-install")
   (require 'org-install)
   (add-to-list 'auto-mode-alist ' ("\\.org\\'" . org-mode))
+
   (define-key my-prefix-map "l" 'org-store-link)
   (define-key my-prefix-map "a" 'org-agenda)
-  (setq org-agenda-files (file-expand-wildcards "~/Org/*.org"))
+  (define-key my-prefix-map "r" 'org-remember)
+
+  (setq org-directory "~/Org")
+  (setq org-agenda-files
+        (file-expand-wildcards
+         (expand-file-name "*.org" org-directory)))
+
+  (org-remember-insinuate)
+  (setq org-default-notes-file
+        (expand-file-name "notes.org" org-directory))
+  (setq org-remember-templates
+      '(("Todo" ?t "** TODO %?\n   %i\n   %a\n   %t" nil "Inbox")
+        ("Bug" ?b "** TODO %?   :bug:\n   %i\n   %a\n   %t" nil "Inbox")
+        ("Idea" ?i "** %?\n   %i\n   %a\n   %t" nil "New Ideas")
+        ))
 
   (setq org-log-done t)
   (setq org-startup-truncated nil)
