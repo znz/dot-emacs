2269e65e501d889d63f293efa3ee31eb2ca575a8
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-05-10 14:44:58 +0900

    import my old config

--- wanderlust.d/50draft.el
+++ wanderlust.d/50draft.el
@@ -8,6 +8,15 @@
 ;; 送信前に確認する
 (setq wl-interactive-send t)
 
+;; Envelope-From を切り替え
+(defun my-wl-draft-set-envelope-from ()
+  (setq wl-envelope-from
+        (wl-address-header-extract-address
+         (or
+          (std11-fetch-field "Sender")
+          (std11-fetch-field "From")))))
+(add-hook 'wl-draft-send-hook 'my-wl-draft-set-envelope-from)
+
 ;;; http://lists.airs.net/wl/archive/200003/msg00213.html
 (defun wl-draft-subject-check ()
   (interactive)
@@ -16,6 +25,42 @@
       (error "Abort.")))
 (add-hook 'wl-draft-send-hook 'wl-draft-subject-check)
 
+;; Message-IDのドメインパートにwl-fromを使う。
+(setq wl-message-id-use-wl-from t)
+
+;; 返信時のウィンドウを広くする
+(setq wl-draft-reply-buffer-style 'full)
+
+;; ドラフトを新しいフレームで書く
+(setq wl-draft-use-frame t)
+
+;; 返信時のヘッダに相手の名前を入れない。
+(setq wl-draft-reply-use-address-with-full-name nil)
+
+;; 自分宛てのメールに返信する場合、常に `To:', `Cc:' から
+;; 自分のメールアドレスを削除
+(setq wl-draft-always-delete-myself t)
+
+;; unplugged 状態で送信すると，キュー(`wl-queue-folder')に格納する
+(setq wl-draft-enable-queuing t)
+
+;; `Fcc:' で保存したメッセージを既読にする
+(setq wl-fcc-force-as-read t)
+
+;; unplugged から plugged に変えたときに，キューにあるメッセージを送信する
+(setq wl-auto-flush-queue t)
+
+;; sendlog
+(setq wl-draft-sendlog t) ;; see ~/.elmo/sendlog
+
+;; Wanderlust 13231
+;; MLへのRES投稿でSubjectからML名を取り除くには？
+(setq wl-subject-re-prefix-regexp
+      (concat "^[ \t]*"
+              ;; [Mailing-List: 9999]
+              "\\(\\s(\\(\\S(+\\)[ :]\\([0-9]+\\)\\s)\\)?[ \t]*"
+              ;; Re:
+              "\\([Rr][Ee][:>][ \t]*\\)*[ \t]*"))
 
 ;;; Local Variables:
 ;;; mode: emacs-lisp
