8d1a9b75c054efd95fd0cc1681258c602cbc62f6
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-05-10 14:37:30 +0900

    add wl-draft-subject-check

--- wanderlust.d/50draft.el
+++ wanderlust.d/50draft.el
@@ -8,6 +8,15 @@
 ;; 送信前に確認する
 (setq wl-interactive-send t)
 
+;;; http://lists.airs.net/wl/archive/200003/msg00213.html
+(defun wl-draft-subject-check ()
+  (interactive)
+  (if (and (< (length (std11-field-body "Subject")) 1)
+           (null (y-or-n-p "No subject! Send current draft?")))
+      (error "Abort.")))
+(add-hook 'wl-draft-send-hook 'wl-draft-subject-check)
+
+
 ;;; Local Variables:
 ;;; mode: emacs-lisp
 ;;; coding: utf-8
