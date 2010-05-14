76f13ef2f82c31eac3b96b9ff2349c10e634eb7f
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-05-14 17:37:32 +0900

    add auto-install comment and comment out compatibility-setup

--- init.el.d/10install.el
+++ init.el.d/10install.el
@@ -3,11 +3,12 @@
 ;; (shell-command-to-string (concat "cd " auto-install-directory " && wget -N http://www.emacswiki.org/emacs/download/auto-install.el"))
 ;; (shell-command-to-string (concat "cd " auto-install-directory " && curl -O http://www.emacswiki.org/emacs/download/auto-install.el"))
 ;; (byte-compile-file (expand-file-name "auto-install.el" auto-install-directory))
+;; or (auto-install-from-emacswiki "auto-install.el")
 (static-when (file-directory-p auto-install-directory)
   (add-to-list 'load-path auto-install-directory)
   (require 'auto-install)
   ;;(auto-install-update-emacswiki-package-name t)
-  (auto-install-compatibility-setup) ; 互換性確保
+  ;;(auto-install-compatibility-setup) ; 互換性確保
   )
 
 ;;; Local Variables:
