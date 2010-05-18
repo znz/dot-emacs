926ee054666f7f4e37696b4b1911c0e94e318211
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-05-18 15:28:30 +0900

    rename variable name

--- init.el.d/50font.el
+++ init.el.d/50font.el
@@ -1,10 +1,12 @@
-(static-when
-    (file-exists-p
-     "/usr/share/fonts/truetype/vlgothic/VL-Gothic-Regular.ttf")
-  (let ((font "VL Gothic"))
-    (set-default-font font)
-    (add-to-list 'default-frame-alist `(font . ,font))
-    ))
+(defvar my-font nil)
+(static-cond
+ ((file-exists-p "/usr/share/fonts/truetype/vlgothic/VL-Gothic-Regular.ttf")
+  (setq my-font "VL Gothic"))
+ )
+(when my-font
+  (set-default-font my-font)
+  (add-to-list 'default-frame-alist `(font . ,my-font))
+  )
 
 ;;; Local Variables:
 ;;; mode: emacs-lisp
