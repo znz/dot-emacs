cdf0340d96b3465fb4f598a6b2187b62f74f70ae
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-06-14 18:52:26 +0900

    add my-ca-certificates

--- wanderlust.d/50ssl.el
+++ wanderlust.d/50ssl.el
@@ -1,9 +1,20 @@
+(static-defconst my-ca-certificates
+  (find-if
+   #'file-exists-p
+   (list
+    (expand-file-name "ca-certificates.crt" user-emacs-directory)
+    "/etc/ssl/certs/ca-certificates.crt"
+    )))
+
 (static-when (and (locate-library "ssl")
                   (exec-installed-p "gnutls-cli"))
   ;; ssl 接続に gnutls を使う。
   ;; http://d.hatena.ne.jp/buzztaiki/20090715/1247619151
   (setq ssl-program-name "gnutls-cli")
-  (setq ssl-program-arguments '("-p" service host)))
+  (setq ssl-program-arguments
+        `("--port" service
+          "--x509cafile" ,my-ca-certificates ;; verify certificates
+          host)))
 
 (static-when (locate-library "tls")
   ;; ssl 接続に gnutls を使う。
