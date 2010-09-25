6de87e07a2e43ca51a74fa37f36e8ec006949549
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-09-26 04:14:16 +0900

    use color-theme

---
+++ init.el.d/50color.el
@@ -0,0 +1,10 @@
+;; M-x color-theme-select で試して選べばいい。
+(when (require 'color-theme nil t)
+  (color-theme-initialize)
+  (color-theme-tty-dark))
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
