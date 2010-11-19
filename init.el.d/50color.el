30b06f3c3e4cadfce9f2b01758d96a81a4326d95
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-11-19 18:23:29 +0900

    add (plist-put develock-max-column-plist 'ruby-mode 'w)

--- init.el.d/50color.el
+++ init.el.d/50color.el
@@ -3,6 +3,10 @@
   (color-theme-initialize)
   (color-theme-tty-dark))
 
+(when (require 'develock nil t)
+  (plist-put develock-max-column-plist 'ruby-mode 'w)
+  )
+
 ;;; Local Variables:
 ;;; mode: emacs-lisp
 ;;; coding: utf-8
