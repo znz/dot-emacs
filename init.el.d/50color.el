cd3052a3808e2b54dde2b5a46d8b4b4fb375ccc8
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2011-06-07 06:43:52 +0900

    change color-theme
    tty dark の赤いカーソルは develock が行末の空白を赤くするのと同じ色で
    カーソル位置を見失いやすかったので、カーソルが黄色い dark laptop に変更。

--- init.el.d/50color.el
+++ init.el.d/50color.el
@@ -1,7 +1,7 @@
 ;; M-x color-theme-select で試して選べばいい。
 (when (require 'color-theme nil t)
   (color-theme-initialize)
-  (color-theme-tty-dark))
+  (color-theme-dark-laptop))
 
 (when (require 'develock nil t)
   (plist-put develock-max-column-plist 'ruby-mode 'w)
