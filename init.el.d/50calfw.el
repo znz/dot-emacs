a58f334012dde34c02a7dc40fcb3ce4c3004708c
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2011-07-28 17:00:21 +0900

    update raw urls

--- init.el.d/50calfw.el
+++ init.el.d/50calfw.el
@@ -8,7 +8,7 @@
            (append japanese-holidays local-holidays other-holidays))
      )))
 
-;; (auto-install-from-url "https://github.com/kiwanami/emacs-calfw/raw/master/calfw.el")
+;; (auto-install-from-url "https://raw.github.com/kiwanami/emacs-calfw/master/calfw.el")
 (static-when (locate-library "calfw")
   (eval-after-load "calfw"
     '(progn
@@ -16,7 +16,7 @@
          '(lambda () (interactive) (kill-buffer nil)))
        )))
 
-;; (auto-install-from-url "https://github.com/kiwanami/emacs-calfw/raw/master/calfw-ical.el")
+;; (auto-install-from-url "https://raw.github.com/kiwanami/emacs-calfw/master/calfw-ical.el")
 (static-when (locate-library "calfw-ical")
   (defun my-cfw-open-IT勉強会カレンダー ()
     (interactive)
@@ -26,7 +26,7 @@
   (define-key my-prefix-map "I" 'my-cfw-open-IT勉強会カレンダー)
   )
 
-;;(auto-install-from-url "https://github.com/kiwanami/emacs-calfw/raw/master/calfw-org.el")
+;;(auto-install-from-url "https://raw.github.com/kiwanami/emacs-calfw/master/calfw-org.el")
 (static-when (locate-library "calfw-org")
   (autoload 'cfw:open-org-calendar "calfw-org" nil t)
   (define-key my-prefix-map "C" 'cfw:open-org-calendar)
