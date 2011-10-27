90c4d5bf8339cdbdf3ce8960f32f69600b0f6f63
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2011-10-27 11:29:05 +0900

    use skk-mode instead of skk-auto-fill-mode

--- init.el.d/50skk.el
+++ init.el.d/50skk.el
@@ -5,6 +5,8 @@
         (expand-file-name "ddskk" user-emacs-directory))
   (setq skk-server-host "localhost")
   (setq skk-server-portnum 1178)
+  ;; skk-auto-fill-mode は不要なので skk-mode に変更
+  (global-set-key "\C-xj" 'skk-mode)
   )
 
 ;;; Local Variables:
