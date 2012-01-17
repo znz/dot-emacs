3bf26c06f8322fc4733be581a98f292c94c5d2c3
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2012-01-17 15:20:43 +0900

    reset C-xC-j after load dired-x

--- init.el.d/50skk.el
+++ init.el.d/50skk.el
@@ -7,6 +7,9 @@
   (setq skk-server-portnum 1178)
   ;; skk-auto-fill-mode は不要なので skk-mode に変更
   (global-set-key "\C-xj" 'skk-mode)
+  ;; dired-x が変更してしまうのを元に戻す。
+  (eval-after-load "dired-x"
+    '(global-set-key "\C-x\C-j" 'skk-mode))
   )
 
 ;;; Local Variables:
