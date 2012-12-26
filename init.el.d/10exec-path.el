7faa4cc223106b143351cf7e56e907a71cfce40c
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2012-12-27 08:44:15 +0900

    add /usr/local/bin to exec-path

--- init.el.d/10exec-path.el
+++ init.el.d/10exec-path.el
@@ -1,10 +1,11 @@
-(mapc
- (lambda (path)
-   (when (file-directory-p path)
-     (setenv "PATH" (concat path ":" (getenv "PATH")))
-     (add-to-list 'exec-path path)))
+(defun my-add-to-path (path)
+  (when (file-directory-p path)
+    (setenv "PATH" (concat path ":" (getenv "PATH")))
+    (add-to-list 'exec-path path)))
+(mapc #'my-add-to-path
  (file-expand-wildcards "/opt/local/*bin") ; MacPorts
  )
+(my-add-to-path "/usr/local/bin") ; Homebrew
 
 ;;; Local Variables:
 ;;; mode: emacs-lisp
