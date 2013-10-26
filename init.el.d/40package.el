7b8c08194ffe39b73f03282ac8e3b1760a9d4ea9
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-10-26 19:15:01 +0900

    add my/favorite-packages

--- init.el.d/40package.el
+++ init.el.d/40package.el
@@ -12,4 +12,29 @@
    '("marmalade" . "http://marmalade-repo.org/packages/"))
 
   (package-initialize)
+
+  (setq
+   my/favorite-packages
+   '(
+     elscreen
+     flymake
+     flymake-haml
+     markdown-mode
+     org
+     shell-pop
+     ))
+
+  (defun my-install-packages ()
+    (interactive)
+    (package-refresh-contents)
+    (dolist (package my/favorite-packages)
+      (unless (package-installed-p package)
+	(package-install package)))
+    )
   )
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
