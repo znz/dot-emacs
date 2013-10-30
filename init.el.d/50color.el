2a46829eafbeeb79bf4be7bec30d3b38e2d215d6
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-10-30 17:13:59 +0900

    use change-log-indent-text

--- init.el.d/50color.el
+++ init.el.d/50color.el
@@ -6,6 +6,14 @@
 
 (when (require 'develock nil t)
   (plist-put develock-max-column-plist 'change-log-mode 79)
+  ;; leading 2 or more tabs
+  (setcdr
+   (assoc "^\\([\t ][\t ]+\\)[^\t\n]" develock-change-log-font-lock-keywords)
+   '(1 (if (and (eq (char-after (develock-point-at-bol)) ?\t)
+                (not (= (- (match-end 1) (match-beginning 1) 1)
+                        change-log-indent-text)))
+           'develock-whitespace-2)))
+
   (plist-put develock-max-column-plist 'ruby-mode 'w)
   (custom-set-variables
    '(develock-mode-strings
