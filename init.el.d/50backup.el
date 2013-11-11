8b0aff929aa5b6cdf78e009cba15791f680f82ec
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-11-11 16:38:39 +0900

    setup temporary-file-directory for shared folders

---
+++ init.el.d/50backup.el
@@ -0,0 +1,14 @@
+(add-to-list
+ 'backup-directory-alist
+ (cons "\\`.*/Dropbox/" temporary-file-directory)
+ t)
+(add-to-list
+ 'backup-directory-alist
+ (cons "\\`.*/ownCloud" temporary-file-directory)
+ t)
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
