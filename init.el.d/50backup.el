efa515c6d8bedc562f21c972f613494dc13334fb
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-11-13 18:00:37 +0900

    cleanup regexp and add Library

--- init.el.d/50backup.el
+++ init.el.d/50backup.el
@@ -1,10 +1,14 @@
 (add-to-list
  'backup-directory-alist
- (cons "\\`.*/Dropbox/" temporary-file-directory)
+ (cons "/Dropbox/" temporary-file-directory)
  t)
 (add-to-list
  'backup-directory-alist
- (cons "\\`.*/ownCloud" temporary-file-directory)
+ (cons "/ownCloud" temporary-file-directory)
+ t)
+(add-to-list
+ 'backup-directory-alist
+ (cons "/Library/" temporary-file-directory)
  t)
 
 ;;; Local Variables:
