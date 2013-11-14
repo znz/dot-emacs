2e4c8e2cf3c48328b3b9d46cb886da7254c4deb5
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-11-14 14:57:45 +0900

    editorconfig

---
+++ init.el.d/50editorconfig.el
@@ -0,0 +1,10 @@
+(static-when (executable-find "editorconfig")
+  (add-to-list 'my/favorite-packages 'editorconfig)
+  (load "editorconfig" t)
+  )
+
+;;; Local Variables:
+;;; mode: emacs-lisp
+;;; coding: utf-8
+;;; indent-tabs-mode: nil
+;;; End:
