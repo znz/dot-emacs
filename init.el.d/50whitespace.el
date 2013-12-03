8de6af09f26f8e5d0f1fae1d2209eb677f8acd27
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-12-03 09:22:14 +0900

    hide trailing-whitespace in calendar-mode

--- init.el.d/50whitespace.el
+++ init.el.d/50whitespace.el
@@ -1,6 +1,10 @@
 ;; 行末の不要なスペースを強調表示する
 (setq-default show-trailing-whitespace t)
 
+;; calendar-mode では表示しない
+(add-hook 'calendar-mode-hook
+          (lambda () (setq show-trailing-whitespace nil)))
+
 ;;; Local Variables:
 ;;; mode: emacs-lisp
 ;;; coding: utf-8
