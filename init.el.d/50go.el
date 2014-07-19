932d39ae673f95c4254378f5793ef81e497f9549
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-19 22:37:12 +0900

    godef-jump

--- init.el.d/50go.el
+++ init.el.d/50go.el
@@ -4,6 +4,11 @@
   (when (require 'go-eldoc nil t)
     (add-hook 'go-mode-hook 'go-eldoc-setup))
 
+  (add-hook
+   'go-mode-hook
+   (lambda ()
+     (local-set-key (kbd "M-.") 'godef-jump)))
+
   (let* ((gopath (or (getenv "GOPATH")
 		     (expand-file-name "~/g")))
 	 (go/bin (concat gopath "/bin")))
