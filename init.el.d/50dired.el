346e6872c2f32116dec64f1c3bda1b512b574400
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-24 22:35:14 +0900

    bind wdired

--- init.el.d/50dired.el
+++ init.el.d/50dired.el
@@ -1,6 +1,7 @@
 (let ((gls (executable-find "gls")))
   (when gls
     (setq insert-directory-program gls)))
+(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
 
 ;;; Local Variables:
 ;;; mode: emacs-lisp
