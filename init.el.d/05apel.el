34475d1151133aa2b16500cc17a9a1545af8c2a8
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-25 11:45:07 +0900

    import GPL-2+ program

--- init.el.d/05apel.el
+++ init.el.d/05apel.el
@@ -10,7 +10,37 @@
 
 (eval-when-compile
   (unless (require 'static nil t)
-    (error "need to install apel")))
+    ;; license of program from static.el is GPL-2+, see ../licenses/GPL-2
+    ;; import from static.el after here
+    (put 'static-if 'lisp-indent-function 2)
+    (defmacro static-if (cond then &rest else)
+      "Like `if', but evaluate COND at compile time."
+      (if (eval cond)
+          then
+        (` (progn  (,@ else)))))
+
+    (put 'static-when 'lisp-indent-function 1)
+    (defmacro static-when (cond &rest body)
+      "Like `when', but evaluate COND at compile time."
+      (if (eval cond)
+          (` (progn (,@ body)))))
+
+    (put 'static-unless 'lisp-indent-function 1)
+    (defmacro static-unless (cond &rest body)
+      "Like `unless', but evaluate COND at compile time."
+      (if (eval cond)
+          nil
+        (` (progn (,@ body)))))
+
+    (defmacro static-cond (&rest clauses)
+      "Like `cond', but evaluate CONDITION part of each clause at compile time."
+      (while (and clauses
+                  (not (eval (car (car clauses)))))
+        (setq clauses (cdr clauses)))
+      (if clauses
+          (cons 'progn (cdr (car clauses)))))
+    ;; import from static.el before here
+    ))
 
 ;;; Local Variables:
 ;;; mode: emacs-lisp
