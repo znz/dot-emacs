c1c243b20457e5d8dc6c4af06612eabca4faa234
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-04-11 16:47:18 +0900

    Remove paredit

--- init.el.d/50paredit.el
+++
@@ -1,4 +0,0 @@
-(add-to-list 'my/favorite-packages 'paredit)
-(when (require 'paredit nil t)
-  (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-  (add-hook 'lisp-interacton-mode-hook 'enable-paredit-mode))
