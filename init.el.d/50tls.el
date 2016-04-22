f39625b4553fa905ccfd47d2a41877b142df5f8b
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-04-22 23:54:38 +0900

    Set secure tls-program

---
+++ init.el.d/50tls.el
@@ -0,0 +1,7 @@
+(static-when (file-exists-p "/etc/ssl/certs/ca-certificates.crt")
+  ;; see doc string of tls-checktrust
+  (setq tls-program
+        '("gnutls-cli --x509cafile /etc/ssl/certs/ca-certificates.crt -p %p %h"
+          "gnutls-cli --x509cafile /etc/ssl/certs/ca-certificates.crt -p %p %h --protocols ssl3"
+          "openssl s_client -connect %h:%p -CAfile /etc/ssl/certs/ca-certificates.crt -no_ssl2 -ign_eof"))
+  )
