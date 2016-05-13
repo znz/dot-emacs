c4ef43565c0308ae7054b2fab86e8703f9600037
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-05-13 22:02:49 +0900

    Fix warning of obsolete variable and update popwin.el
    Warning: `display-buffer-function' is an obsolete variable (as
    of 24.3); use `display-buffer-alist' instead.

--- init.el.d/50popwin.el
+++ init.el.d/50popwin.el
@@ -1,4 +1,4 @@
 ;; (auto-install-from-url "https://github.com/m2ym/popwin-el/raw/master/popwin.el")
 
 (when (require 'popwin nil t)
-  (setq display-buffer-function 'popwin:display-buffer))
+  (popwin-mode 1))
