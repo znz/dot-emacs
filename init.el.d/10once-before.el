6285946a09d4805224e883303ad20514294ee4fd
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-05-15 20:13:30 +0900

    Use advice-add if available

--- init.el.d/10once-before.el
+++ init.el.d/10once-before.el
@@ -1,8 +1,17 @@
 (put 'my-once-before 'lisp-indent-function 2)
-(defmacro my-once-before (function name body)
-  "functionの初回実行時だけにbodyを実行するnameという名前のaround adviceを定義する。"
-  `(defadvice ,function (around ,name activate)
-     ,body
-     ad-do-it
-     (ad-remove-advice ',function 'around ',name)
-     (ad-update ',function)))
+(static-if (fboundp 'advice-add)
+    (defmacro my-once-before (function name body)
+      "functionの初回実行時だけにbodyを実行するnameという名前のaround adviceを定義する。"
+      `(progn
+	 (defun ,name (&rest rest-vars)
+	     ,body
+	   (advice-remove ',function ',name))
+	 (advice-add ',function :before ',name)))
+  (defmacro my-once-before (function name body)
+    "functionの初回実行時だけにbodyを実行するnameという名前のaround adviceを定義する。"
+    `(defadvice ,function (around ,name activate)
+       ,body
+       ad-do-it
+       (ad-remove-advice ',function 'around ',name)
+       (ad-update ',function)))
+  )
