354352e6e2612781be20290d473f7c2f2b88455b
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-09-06 23:51:32 +0900

    Add js2-mode for jsx

--- init.el.d/50javascript.el
+++ init.el.d/50javascript.el
@@ -1,4 +1,8 @@
+(add-to-list 'my/favorite-packages 'js2-mode)
 (static-when (locate-library "js2-mode")
+  (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+  (flycheck-add-mode 'javascript-eslint 'js2-jsx-mode)
+  (add-hook 'js2-jsx-mode-hook 'flycheck-mode)
   (defun my-js2-mode-hook ()
     (setq js2-basic-offset 2)
     (setq js-indent-level 2)
