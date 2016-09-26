dd1edeb4f4b841793e28106b1eb42db9c272234b
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-09-26 18:38:53 +0900

    Follow new saveplace in emacs 25
    see http://qiita.com/watson1978/items/7e03e3f2bd974abff177

--- init.el.d/50session.el
+++ init.el.d/50session.el
@@ -22,6 +22,8 @@
   (add-hook 'after-init-hook 'session-initialize))
  ((locate-library "saveplace")
   (require 'saveplace)
-  (setq-default save-place t)
+  (if (fboundp 'save-place-mode)
+      (save-place-mode 1) ; emacs >= 25
+    (setq-default save-place t)) ; emacs < 25
   (setq save-place-file
         (expand-file-name "save-place.el" temporary-file-directory))))
