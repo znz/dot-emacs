01f1c45d6a478c3ce3d572077bade13b9748ca5e
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-12-17 17:12:27 +0900

    Add defun for pinentry-emacs

---
+++ init.el.d/50gnupg.el
@@ -0,0 +1,4 @@
+;; https://github.com/ecraven/pinentry-emacs
+(defun pinentry-emacs (desc prompt ok error)
+  (let ((str (read-passwd (concat (replace-regexp-in-string "%22" "\"" (replace-regexp-in-string "%0A" "\n" desc)) prompt ": "))))
+    str))
