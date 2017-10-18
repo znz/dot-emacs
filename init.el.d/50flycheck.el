8a91ae4fcfd4e0cf73f6bd0e0ce8a100ff291113
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2017-10-18 11:44:18 +0900

    Change prefix of flycheck
    because `C-c !` conflicts org-mode's

--- init.el.d/50flycheck.el
+++ init.el.d/50flycheck.el
@@ -1,3 +1,5 @@
 (add-to-list 'my/favorite-packages 'flycheck)
 (static-when (fboundp 'global-flycheck-mode)
+  (with-eval-after-load 'flycheck
+    (custom-set-variables '(flycheck-keymap-prefix (kbd "C-c f"))))
   (add-hook 'after-init-hook #'global-flycheck-mode))
