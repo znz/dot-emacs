db003262275af70bc184444631b636cb9d572f45
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2017-12-12 20:56:08 +0900

    Update go get URLs and add hook

--- init.el.d/50go.el
+++ init.el.d/50go.el
@@ -1,11 +1,12 @@
 (add-to-list 'my/favorite-packages 'go-mode)
 (add-to-list 'my/favorite-packages 'go-eldoc)
 (when (require 'go-mode nil t)
+  ;; https://github.com/syohex/emacs-go-eldoc
   ;; go get -u github.com/nsf/gocode
   (when (require 'go-eldoc nil t)
     (add-hook 'go-mode-hook 'go-eldoc-setup))
 
-  ;; go get -u code.google.com/p/rog-go/exp/cmd/godef
+  ;; go get -u github.com/rogpeppe/godef
   (add-hook
    'go-mode-hook
    (lambda ()
@@ -13,13 +14,16 @@
 
 
   (let* ((gopath (or (getenv "GOPATH")
-		     (expand-file-name "~/g")))
-	 (go/bin (concat gopath "/bin")))
+                   (expand-file-name "~/g")))
+   (go/bin (concat gopath "/bin")))
     (when (file-directory-p gopath)
       (setenv "GOPATH" gopath)
       (my-add-to-path go/bin)))
 
-  ;; go get -u code.google.com/p/go.tools/cmd/goimports
+  ;; https://godoc.org/golang.org/x/tools/cmd/goimports
+  ;; go get -u golang.org/x/tools/cmd/goimports
   (static-when (executable-find "goimports")
     (setq gofmt-command "goimports"))
+
+  (add-hook 'before-save-hook 'gofmt-before-save)
   )
