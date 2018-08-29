982514c342a452ca2446697786483c3439eb8de2
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2018-08-29 23:37:33 +0900

    Add textlint

--- init.el.d/50flycheck.el
+++ init.el.d/50flycheck.el
@@ -1,6 +1,19 @@
 (add-to-list 'my/favorite-packages 'flycheck)
 (static-when (fboundp 'global-flycheck-mode)
   (with-eval-after-load 'flycheck
+    (flycheck-define-checker textlint
+      "A linter for prose."
+      :command ("textlint" "--format" "unix"
+                 ;; "--rule" "no-mix-dearu-desumasu" "--rule" "max-ten" "--rule" "spellcheck-tech-word"
+                 source-inplace)
+      :error-patterns
+      ((warning line-start (file-name) ":" line ":" column ": "
+         (id (one-or-more (not (any " "))))
+         (message (one-or-more not-newline)
+           (zero-or-more "\n" (any " ") (one-or-more not-newline)))
+         line-end))
+      :modes (text-mode markdown-mode gfm-mode))
+    (add-to-list 'flycheck-checkers 'textlint)
     (custom-set-variables '(flycheck-keymap-prefix (kbd "C-c f"))))
   (setq flycheck-command-wrapper-function
     (lambda (command)
@@ -25,3 +38,6 @@
   (add-hook 'js-mode-hook #'add-node-modules-path))
 (with-eval-after-load 'js2-mode
   (add-hook 'js2-mode-hook #'add-node-modules-path))
+(with-eval-after-load 'markdown-mode
+  (add-hook 'gfm-mode-hook #'add-node-modules-path)
+  (add-hook 'markdown-mode-hook #'add-node-modules-path))
