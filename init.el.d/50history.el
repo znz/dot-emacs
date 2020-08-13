d19526830201e32db88574f15aff09d0a23cc7ab
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2020-08-13 12:31:29 +0900

    Fix `Package cl is deprecated`

--- init.el.d/50history.el
+++ init.el.d/50history.el
@@ -4,7 +4,6 @@
 ;; http://homepage3.nifty.com/akima/soft/meadow_20.html#SEC293
 ;; 20.2.5 ヒストリから重複を削除 (2003/01/21)
 ;; history から重複したのを消す
-(eval-when-compile (require 'cl))
 (defun minibuffer-delete-duplicate ()
   (when (listp minibuffer-history-variable)
     (let (list)
